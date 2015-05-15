df117dbaabe4dae438523ac1b14d4204d8e1a1b9
Mandar Haldekar <mandar.haldekar@gmail.com>
2015-05-15 10:35:55 -0400

    Geofence radius changed to 600 meters

--- app/src/main/java/com/mobilecomputing/mandar/contextawareapp/Constants.java
+++ app/src/main/java/com/mobilecomputing/mandar/contextawareapp/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
      */
     public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
             GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
-    public static final float GEOFENCE_RADIUS_IN_METERS = 300; // 1 mile, 1.6 km
+    public static final float GEOFENCE_RADIUS_IN_METERS = 600; // in meters
     public static final String HOME_GEOFENCE = "HOME";
     public static final String WORK_GEOFENCE = "WORK";
     public static final String GEOFENCE_TYPE = "TYPE";
